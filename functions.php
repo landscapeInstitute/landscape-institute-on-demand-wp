23cfb4270060b660c39f065fe895a0f788670bca
Louis Varley <32775301+li-louisvarley@users.noreply.github.com>
2019-08-08 14:28:50 +0100

    Version: 1.3

---
+++ functions.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+Plugin Name: Landscape Institute | On-Demand WP
+Plugin URI: https://github.com/landscapeInstitute/landscape-institute-on-demand-wp
+Description: Provide OnDemand services via events, videos, purchases and subscriptions
+Version: 1.3
+Author: Louis Varley
+Author URI: http://www.landscapeinstitute.org
+*/
+/*
+	Copyright 2013	Landscape Institute	(email : louis.varley@landscapeinstitute.org)
+	Licensed under the GPLv2 license: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/* Defines */
+define('LIOD_ROOT',$_SERVER['DOCUMENT_ROOT']);
+define('LIOD_PLUGIN_PATH',plugin_dir_path(__FILE__));
+define('LIOD_CLASSES_PATH',LIOD_PLUGIN_PATH . 'classes/');
+
+/* Updater */
+
+require('updater.php');
+
+add_action('admin_init',function(){
+	new WP_GitHub_Updater(__FILE__);
+});
+
+
+/* CMB2 */
+require_once(LIOD_PLUGIN_PATH . 'cmb2.php');
+
+/* Helpers */
+require_once(LIOD_PLUGIN_PATH . 'helpers/functions.php');
+require_once(LIOD_PLUGIN_PATH . 'helpers/extensions.php');
+
+/* Composer */
+$composer = require_once(LIOD_PLUGIN_PATH . 'vendor/autoload.php');
+
+/* Plugin Init */
+add_action('init',function(){
+	liod()->init();
+    do_action('liod_init');
+});
+
+/* Admin Init */
+add_action('admin_init',function(){
+	liod()->admin_init();
+    do_action('liod_admin_init');
+});
+
+
+/* Helper function for getting the main instance */
+function liod(){	
+	return \liod\core\core::instance();
+}
